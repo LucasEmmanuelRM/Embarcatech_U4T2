9b788063dec436e3a51fb258e52cb9a6d2ef7286
Nerinho Nascimento <neramornas@gmail.com>
2025-01-19 06:49:52 -0300

    start: Criação de branch
    Signed-off-by: Nerinho Nascimento <neramornas@gmail.com>

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Lucas/Desktop/Embarcatech_U4T2
+# Install script for directory: C:/Workspace/Embarcatech/EmbarcaTech_U4T2/Embarcatech_U4T2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/Lucas/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/Windows 10/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/Lucas/Desktop/Embarcatech_U4T2/build/pico-sdk/cmake_install.cmake")
+  include("C:/Workspace/Embarcatech/EmbarcaTech_U4T2/Embarcatech_U4T2/build/pico-sdk/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Lucas/Desktop/Embarcatech_U4T2/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Workspace/Embarcatech/EmbarcaTech_U4T2/Embarcatech_U4T2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- build/pico-sdk/src/rp2_common/pico_cyw43_driver/cmake_install.cmake
+++ build/pico-sdk/src/rp2_common/pico_cyw43_driver/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Lucas/.pico-sdk/sdk/2.1.0/src/rp2_common/pico_cyw43_driver
+# Install script for directory: C:/Users/Windows 10/.pico-sdk/sdk/2.1.0/src/rp2_common/pico_cyw43_driver
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,11 +34,11 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/Lucas/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/Windows 10/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Lucas/Desktop/Embarcatech_U4T2/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cmake_install.cmake")
+  include("C:/Workspace/Embarcatech/EmbarcaTech_U4T2/Embarcatech_U4T2/build/pico-sdk/src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cmake_install.cmake")
 endif()
 
